1aa58fa188a56277d45202263a1d847c8b8ed449
Austin Baker <secondbaker@Austins-MacBook.local>
2020-06-07 18:06:35 -0400

    I needed to make sure the button's function was added after the button was added to the page.

--- calc-function.js
+++ calc-function.js
@@ -6,88 +6,85 @@ const CaloriesPerCarbohydrateGram = 4;
 $(document).ready(insertCalculator);
 
 function insertCalculator() {
-    console.log('inserting?');
     $('.nutritional-calculator').load("/calculator.html", function(data) {
-        console.log('got data');
         $(".nutritional-calculator").html(data);
-        console.log(data);
+        $('#calculate-button').click(button =>
+            {
+                console.log("clicked");
+                let weight = weightToPounds($('#weight').val(), $('#unit').children("option:selected").val());
+            
+                $('#nutrition-tables').children().each(function fillTable() {
+                    let classes = $(this).attr('class');
+                    try
+                    {
+                        //first we'll figure out the calories needed
+                        let calories = 0;
+                        if(classes.includes('maintain'))
+                        {
+                            calories = weight * 14;
+                        }
+                        else if(classes.includes('lose-fat'))
+                        {
+                            calories = weight * 12;
+                        }
+                        else if(classes.includes('gain-muscle'))
+                        {
+                            calories = weight * 16;
+                        }
+                        $(this).find('.calories').html(calories);
+            
+                        //next we'll figure out the protein, since it relies only on weight
+                        let proteinGrams = 0;
+                        let proteinPalms = 0;
+                        if(classes.includes('high-protein'))
+                        {
+                            proteinGrams = weight * 1.5;
+                        }
+                        else
+                        {
+                            proteinGrams = weight;
+                        }
+                        proteinPalms = proteinGrams / 30;
+                        $(this).find('.protein-grams').html(proteinGrams);
+                        $(this).find('.protein-palms').html(proteinPalms);
+            
+                        //next we'll need to get the carbohydrates
+                        //carbohydrates and fats will be calculated based on a percentage of what calories remain
+                        let fatAndCarbCalories = calories - (proteinGrams * CaloriesPerProteinGram);
+                        console.log(fatAndCarbCalories);
+                        let carbCalories = 0;
+                        if(classes.includes('low-carb'))
+                        {
+                            carbCalories = fatAndCarbCalories / 4;
+                        }
+                        else
+                        {
+                            carbCalories = fatAndCarbCalories / 2;
+                        }
+            
+                        let carbohydratesGrams = carbCalories / CaloriesPerCarbohydrateGram;
+                        let carbohydratesCuppedHandfulls = Math.round(carbohydratesGrams / 35);
+                        $(this).find('.carbohydrates-grams').html(carbohydratesGrams);
+                        $(this).find('.carbohydrates-cupped-handfulls').html(carbohydratesCuppedHandfulls);
+            
+                        //finally we get the fats
+                        let fatCalories = fatAndCarbCalories - carbCalories;
+                        let fatGrams = fatCalories / CaloresPerFatGram;
+                        let fatThumbs = fatGrams / 15;
+                        $(this).find('.fats-grams').html(fatGrams);
+                        $(this).find('.fats-thumbs').html(fatThumbs);
+                    }
+                    catch(error)
+                    {
+                        //This is where we end up if classes is undefined
+                    }
+                });
+            
+            });
     }); 
-    console.log('should have inserted');
 }
 
-$('#calculate-button').click(button =>
-{
-    console.log("clicked");
-    let weight = weightToPounds($('#weight').val(), $('#unit').children("option:selected").val());
 
-    $('#nutrition-tables').children().each(function fillTable() {
-        let classes = $(this).attr('class');
-        try
-        {
-            //first we'll figure out the calories needed
-            let calories = 0;
-            if(classes.includes('maintain'))
-            {
-                calories = weight * 14;
-            }
-            else if(classes.includes('lose-fat'))
-            {
-                calories = weight * 12;
-            }
-            else if(classes.includes('gain-muscle'))
-            {
-                calories = weight * 16;
-            }
-            $(this).find('.calories').html(calories);
-
-            //next we'll figure out the protein, since it relies only on weight
-            let proteinGrams = 0;
-            let proteinPalms = 0;
-            if(classes.includes('high-protein'))
-            {
-                proteinGrams = weight * 1.5;
-            }
-            else
-            {
-                proteinGrams = weight;
-            }
-            proteinPalms = proteinGrams / 30;
-            $(this).find('.protein-grams').html(proteinGrams);
-            $(this).find('.protein-palms').html(proteinPalms);
-
-            //next we'll need to get the carbohydrates
-            //carbohydrates and fats will be calculated based on a percentage of what calories remain
-            let fatAndCarbCalories = calories - (proteinGrams * CaloriesPerProteinGram);
-            console.log(fatAndCarbCalories);
-            let carbCalories = 0;
-            if(classes.includes('low-carb'))
-            {
-                carbCalories = fatAndCarbCalories / 4;
-            }
-            else
-            {
-                carbCalories = fatAndCarbCalories / 2;
-            }
-
-            let carbohydratesGrams = carbCalories / CaloriesPerCarbohydrateGram;
-            let carbohydratesCuppedHandfulls = Math.round(carbohydratesGrams / 35);
-            $(this).find('.carbohydrates-grams').html(carbohydratesGrams);
-            $(this).find('.carbohydrates-cupped-handfulls').html(carbohydratesCuppedHandfulls);
-
-            //finally we get the fats
-            let fatCalories = fatAndCarbCalories - carbCalories;
-            let fatGrams = fatCalories / CaloresPerFatGram;
-            let fatThumbs = fatGrams / 15;
-            $(this).find('.fats-grams').html(fatGrams);
-            $(this).find('.fats-thumbs').html(fatThumbs);
-        }
-        catch(error)
-        {
-            //This is where we end up if classes is undefined
-        }
-    });
-
-});
 
 //makes sure the weight unit is pounds, regardless of which unit is selected to begin with
 function weightToPounds (weight, unit)
