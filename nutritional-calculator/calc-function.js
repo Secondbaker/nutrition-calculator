10e876b9371b0c92c667c7cedfc4536c2ae80c1f
secondbaker <secondbaker@gmail.com>
2020-06-10 10:31:50 -0400

    Cleaning up the js and adding comments.

--- nutritional-calculator/calc-function.js
+++ nutritional-calculator/calc-function.js
@@ -7,46 +7,50 @@ const ProteinPerPalm = 30.0;
 const CarbohydratesPerCuppedHandfull = 35.0;
 const FatPerThumb = 15.0;
 
+//We need to get the absolute position of this script to link to adjacent files
+//When document.getElementsByTagName("script") is called, this will be the last script in the array
 var scripts = document.getElementsByTagName("script");
 var src = scripts[scripts.length-1].src;
+//cut off this file's name so src is the name of its folder
 src = src.substr(0, src.indexOf('calc-function.js'));
 
-$(document).ready(insertCalculator);
+$(document).ready(setup);
 
-function insertCalculator() {
+//setup should be called when the document is ready
+//it adds necessary scripts and css to the page, the inserts the calculator
+function setup() {
     // Get HTML head element 
     var head = document.getElementsByTagName('HEAD')[0];  
   
-    // Create new link Element 
-    var cssLink = document.createElement('link'); 
-
     //Add my stylesheet
-    // set the attributes for link element  
+    var cssLink = document.createElement('link'); 
     cssLink.rel = 'stylesheet';  
     cssLink.type = 'text/css'; 
     cssLink.href = src + 'nutritional-calculator-styles.css';
-    // Append link element to HTML head 
-    head.appendChild(cssLink); 
+    head.appendChild(cssLink);
 
-    var fitTextLink = document.createElement('link'); 
-    //Add FitText
-    fitTextLink.rel = 'script';
-    fitTextLink.type = 'text/js';
-    fitTextLink.href = src + 'jquery.fittext.js';
-    head.appendChild(fitTextLink);
-   
+    insertCalculator();
+
+}
+
+function insertCalculator() {
+
     $('.nutritional-calculator').load(src + "calculator.html", function(data) {
+        
         //Put the calculator in the page
         $(".nutritional-calculator").html(data);
-        //Add the click function to the calculator
+        
+        //Add the click function to the calculator and make it so all the inputs update it
         $('#calculate-button').click(calculate);
         $('#weight').on('input', calculate);
         $('#unit').change(calculate);
+        
+        //Resize the text in the tables using FitText
         $('.item-label').each(function() {
-            $(this).fitText(1.5);
+            $(this).fitText(1.5);  //Squish more
         });
         $('.item-number').each(function() {
-            $(this).fitText(.6);
+            $(this).fitText(.6);  //Squish less
         });
         $('.item-unit').each(function() {
             $(this).fitText(.8);
@@ -60,92 +64,93 @@ function insertCalculator() {
 
 function calculate ()
 {
-        let weight = weightToPounds($('#weight').val(), $('#unit').children("option:selected").val());
-    
-        $('.nutrition-tables').children().each(function fillTable() {
-            let classes = $(this).attr('class');
-            try
-            {
-                //first we'll figure out the calories needed
-                let calories = 0;
-                if(classes.includes('maintain'))
-                {
-                    calories = weight * 14.0;
-                }
-                else if(classes.includes('lose-fat'))
-                {
-                    calories = weight * 12.0;
-                }
-                else if(classes.includes('gain-muscle'))
-                {
-                    calories = weight * 16.0;
-                }
-                $(this).find('.calories').html(roundToNearest(calories, RoundingFactor));
-    
-                //next we'll figure out the protein, since it relies only on weight
-                let proteinGrams = 0;
-                let proteinPalms = 0;
-                if(classes.includes('high-protein'))
-                {
-                    proteinGrams = weight * 1.5;
-                }
-                else
-                {
-                    proteinGrams = weight;
-                }
-                proteinPalms = proteinGrams / ProteinPerPalm;
-                $(this).find('.protein-grams').html(roundToNearest(proteinGrams, RoundingFactor));
-                $(this).find('.protein-palms').html(roundToNearest(proteinPalms, RoundingFactor));
-    
-                //next we'll need to get the carbohydrates
-                //carbohydrates and fats will be calculated based on a percentage of what calories remain
-                let fatAndCarbCalories = calories - (proteinGrams * CaloriesPerProteinGram);
-                let carbCalories = 0;
-                if(classes.includes('low-carb'))
-                {
-                    carbCalories = fatAndCarbCalories / 4.0;
-                }
-                else
-                {
-                    carbCalories = fatAndCarbCalories / 2.0;
-                }
-    
-                let carbohydratesGrams = carbCalories / CaloriesPerCarbohydrateGram;
-                let carbohydratesCuppedHandfulls = carbohydratesGrams / CarbohydratesPerCuppedHandfull;
-                $(this).find('.carbohydrates-grams').html(roundToNearest(carbohydratesGrams, RoundingFactor));
-                $(this).find('.carbohydrates-cupped-handfulls').html(roundToNearest(carbohydratesCuppedHandfulls, RoundingFactor));
-    
-                //veggies are based on different things depending on which
-                //diet plan we're looking at
-                let veggiesFists = 0;
-                if(classes.includes('low-carb'))
-                {
-                    veggiesFists = carbohydratesCuppedHandfulls * 3;
-                }
-                else if(classes.includes('high-protein') && !classes.includes('low-carb'))
-                {
-                    veggiesFists = carbohydratesCuppedHandfulls * 1.5;
-                }
-                else
-                {
-                    veggiesFists = carbohydratesCuppedHandfulls;
-                }
-                $(this).find('.veggies-fists').html(roundToNearest(veggiesFists, RoundingFactor));
+    //Grab the weight from the input
+    let weight = weightToPounds($('#weight').val(), $('#unit').children("option:selected").val());
 
-                //finally we get the fats
-                let fatCalories = fatAndCarbCalories - carbCalories;
-                let fatGrams = fatCalories / CaloresPerFatGram;
-                let fatThumbs = fatGrams / FatPerThumb;
-                $(this).find('.fats-grams').html(roundToNearest(fatGrams, RoundingFactor));
-                $(this).find('.fats-thumbs').html(roundToNearest(fatThumbs, RoundingFactor));
-            }
-            catch(error)
+    $('.nutrition-tables').children().each(function fillTable() {
+        let classes = $(this).attr('class');
+        try
+        {
+            //first we'll figure out the calories needed
+            let calories = 0;
+            if(classes.includes('maintain'))
             {
-                //This is where we end up if classes is undefined
+                calories = weight * 14.0;
             }
-        });
-    
-    
+            else if(classes.includes('lose-fat'))
+            {
+                calories = weight * 12.0;
+            }
+            else if(classes.includes('gain-muscle'))
+            {
+                calories = weight * 16.0;
+            }
+            $(this).find('.calories').html(roundToNearest(calories, RoundingFactor));
+
+            //next we'll figure out the protein, since it relies only on weight
+            let proteinGrams = 0;
+            let proteinPalms = 0;
+            if(classes.includes('high-protein'))
+            {
+                proteinGrams = weight * 1.5;
+            }
+            else
+            {
+                proteinGrams = weight;
+            }
+            proteinPalms = proteinGrams / ProteinPerPalm;
+            $(this).find('.protein-grams').html(roundToNearest(proteinGrams, RoundingFactor));
+            $(this).find('.protein-palms').html(roundToNearest(proteinPalms, RoundingFactor));
+
+            //next we'll need to get the carbohydrates
+            //carbohydrates and fats will be calculated based on a percentage of what calories remain
+            let fatAndCarbCalories = calories - (proteinGrams * CaloriesPerProteinGram);
+            let carbCalories = 0;
+            if(classes.includes('low-carb'))
+            {
+                carbCalories = fatAndCarbCalories / 4.0;
+            }
+            else
+            {
+                carbCalories = fatAndCarbCalories / 2.0;
+            }
+
+            let carbohydratesGrams = carbCalories / CaloriesPerCarbohydrateGram;
+            let carbohydratesCuppedHandfulls = carbohydratesGrams / CarbohydratesPerCuppedHandfull;
+            $(this).find('.carbohydrates-grams').html(roundToNearest(carbohydratesGrams, RoundingFactor));
+            $(this).find('.carbohydrates-cupped-handfulls').html(roundToNearest(carbohydratesCuppedHandfulls, RoundingFactor));
+
+            //veggies are based on different things depending on which
+            //diet plan we're looking at
+            let veggiesFists = 0;
+            if(classes.includes('low-carb'))
+            {
+                veggiesFists = carbohydratesCuppedHandfulls * 3;
+            }
+            else if(classes.includes('high-protein') && !classes.includes('low-carb'))
+            {
+                veggiesFists = carbohydratesCuppedHandfulls * 1.5;
+            }
+            else
+            {
+                veggiesFists = carbohydratesCuppedHandfulls;
+            }
+            $(this).find('.veggies-fists').html(roundToNearest(veggiesFists, RoundingFactor));
+
+            //finally we get the fats
+            let fatCalories = fatAndCarbCalories - carbCalories;
+            let fatGrams = fatCalories / CaloresPerFatGram;
+            let fatThumbs = fatGrams / FatPerThumb;
+            $(this).find('.fats-grams').html(roundToNearest(fatGrams, RoundingFactor));
+            $(this).find('.fats-thumbs').html(roundToNearest(fatThumbs, RoundingFactor));
+        }
+        catch(error)
+        {
+            //This is where we end up if classes is undefined
+        }
+    });
+
+
 }
 
 //returns a number rounded to nearest roundTo
